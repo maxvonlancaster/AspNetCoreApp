b0d3321863156257890177f9317520b0f480063b
maxvonlancaster <windchange7@gmail.com>
2020-06-23 13:05:11 +0300

    questions entity

--- AspNetCoreApp.DAL/Entities/Question.cs
+++ AspNetCoreApp.DAL/Entities/Question.cs
@@ -11,5 +11,10 @@ namespace AspNetCoreApp.DAL.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        public Presentation Presentation { get; set; }
+        public string Sentence { get; set; }
+        public string Options { get; set; }
+        public int Correct { get; set; }
+        public int Slide { get; set; }
     }
 }
