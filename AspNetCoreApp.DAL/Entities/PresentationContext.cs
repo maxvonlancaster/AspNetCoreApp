3cae58139dba66778216189e766721e899404250
vamelnyk <vamelnyk@homeaway.com>
2020-06-23 13:01:12 +0300

    entities started

---
+++ AspNetCoreApp.DAL/Entities/PresentationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreApp.DAL.Entities
+{
+    public class PresentationContext : DbContext
+    {
+        public PresentationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Presentation> Presentations { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
---
+++ AspNetCoreApp.DAL/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AspNetCoreApp.DAL.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string TelegramId { get; set; }
+    }
+}
