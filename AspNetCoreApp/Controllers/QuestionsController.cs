978eb1b04272c477d3dc96b92b0bac37a1eaf267
maxvonlancaster <windchange7@gmail.com>
2020-07-10 17:16:49 +0300

    questions index page started

---
+++ AspNetCoreApp/Controllers/QuestionsController.cs
@@ -0,0 +1,32 @@
+﻿using AspNetCoreApp.BLL.Interfaces;
+using AspNetCoreApp.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreApp.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        private readonly IQuestionService _questionService;
+
+        public QuestionsController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int presentationId) 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddQuestion([FromBody]Question question) 
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
