a693226a19fcd3f40a5f1bce7803cb970a30b5a6
maxvonlancaster <windchange7@gmail.com>
2020-10-10 19:21:05 +0300

    empty components

--- AspNetCoreApp/wwwroot/Scripts/app.js
+++ AspNetCoreApp/wwwroot/Scripts/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Streaming from './components/streaming/Streaming.js';
 import Main from './components/main/Main.js';
+import AddQuestion from './components/questions/AddQuestion.js'
 
 ReactDOM.render(
     <BrowserRouter>
@@ -10,6 +11,7 @@ ReactDOM.render(
             <Switch>
                 <Route exact path="/" component={Main} />
                 <Route path="/streaming" component={Streaming} />
+                <Route path="/addQuestion" component={AddQuestion} /> 
             </Switch>
         </main>
     </BrowserRouter>,
---
+++ AspNetCoreApp/wwwroot/Scripts/components/questions/AddQuestion.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+
+class AddQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+    }
+
+    render() {
+        return (
+            <div>This is section for adding questions</div>
+        )
+    }
+
+}
+
+export default AddQuestion;
