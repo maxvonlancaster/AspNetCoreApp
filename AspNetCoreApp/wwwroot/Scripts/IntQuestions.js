fce417c2397cebb879020ac776a80b15a750ebd7
maxvonlancaster <windchange7@gmail.com>
2020-10-11 00:20:03 +0300

    interview questions

---
+++ AspNetCoreApp/wwwroot/Scripts/IntQuestions.js
@@ -0,0 +1,24 @@
+﻿//JS:
+//Difference between var, let, const
+//    How hoisting works
+//Benefit of Immediately Invoked Function Expression
+//Array method.reduce() how works
+//Arrow function vs function
+//.call(), .apply(), .bind() what is it
+//Event bubbling vs Event capturing
+//Promise states
+//Event loop(micro and macro tasks)
+//Docs to read:
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
+//https://javascript.info/
+//React:
+//React lifecycles for Mounting, Updating, Unmounting
+//forceUpdate method to rerender component
+//Attribute 'key' for iterations.Why it's so important to have it
+//How VirtualDOM works in React
+//React doc - https://reactjs.org/docs/getting-started.html
+//(all info is included)
+//Web:
+//Cookie structure ? https ://flaviocopes.com/cookies/
+//    Sessionstorage vs localstorage
+//CORS 
