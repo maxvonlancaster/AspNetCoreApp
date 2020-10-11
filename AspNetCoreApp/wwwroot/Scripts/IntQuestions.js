312ab27b09edf94e23cf5a9867f817f8b01ae637
maxvonlancaster <windchange7@gmail.com>
2020-10-11 18:06:01 +0300

    diff var let const

--- AspNetCoreApp/wwwroot/Scripts/IntQuestions.js
+++ AspNetCoreApp/wwwroot/Scripts/IntQuestions.js
@@ -1,24 +1,121 @@
-﻿//JS:
-//Difference between var, let, const
-//    How hoisting works
-//Benefit of Immediately Invoked Function Expression
-//Array method.reduce() how works
-//Arrow function vs function
-//.call(), .apply(), .bind() what is it
-//Event bubbling vs Event capturing
-//Promise states
-//Event loop(micro and macro tasks)
-//Docs to read:
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript
-//https://javascript.info/
-//React:
-//React lifecycles for Mounting, Updating, Unmounting
-//forceUpdate method to rerender component
-//Attribute 'key' for iterations.Why it's so important to have it
-//How VirtualDOM works in React
-//React doc - https://reactjs.org/docs/getting-started.html
-//(all info is included)
-//Web:
-//Cookie structure ? https ://flaviocopes.com/cookies/
-//    Sessionstorage vs localstorage
-//CORS 
+﻿export default class Fundamentals {
+    //JS:
+    //Difference between var, let, const
+    Difference() {
+        // var declarations are globally scoped or function/locally scoped.
+        // The scope is global when a var variable is declared outside a function.This means that any 
+        // variable that is declared with var outside a function block is available for use in the whole window.
+        // var variables can be re-declared and updated
+        var greeter = "hey hi";
+        var times = 4;
+        if (times > 3) {
+            var greeter = "say Hello instead";
+        }
+        console.log(greeter) // "say Hello instead" -- problem with var 
+        // If you have used greeter in other parts of your code, you might be surprised at the output you might get.
+
+        // let is block scoped. A block is a chunk of code bounded by { }.A block lives in curly braces.
+        // Anything within curly braces is a block.
+        let greeting = "say Hi";
+        let times = 4;
+        if (times > 3) {
+            let hello = "say Hello instead";
+            console.log(hello);// "say Hello instead"
+        }
+        console.log(hello) // hello is not defined
+        // ust like  var, let declarations are hoisted to the top. Unlike var which is initialized as undefined, 
+        // the let keyword is not initialized.So if you try to use a let variable before declaration, you'll get a Reference Error.
+
+        // Variables declared with the const maintain constant values. const declarations share some similarities with let declarations. 
+        // Const is block scoped, const cannot be updated or re-declared
+        const hi = "say Hi";
+        hi = "say Hello instead";// error: Assignment to constant variable. 
+    }
+
+    //How hoisting works
+    Hoisting() {
+
+    }
+
+    //Benefit of Immediately Invoked Function Expression
+    ImmeiatelyInvokedFunct() {
+
+    }
+
+    //Array method.reduce() how works
+    ArrayReduce() {
+
+    }
+
+    //Arrow function vs function
+    ArrowFunVSFunc() {
+
+    }
+
+    //.call(), .apply(), .bind() what is it
+    CallApplyBind() {
+
+    }
+
+    //Event bubbling vs Event capturing
+    EventBubblingCapturing() {
+
+    }
+
+    //Promise states
+    PromiseStates() {
+
+    }
+
+    //Event loop(micro and macro tasks)
+    EventLoop() {
+
+    }
+
+    //Docs to read:
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript
+    //https://javascript.info/
+
+
+    //React:
+    //React lifecycles for Mounting, Updating, Unmounting
+    ReactLifecycles() {
+
+    }
+
+    //forceUpdate method to rerender component
+    ForceUpdateMethod() {
+
+    }
+
+    //Attribute 'key' for iterations.Why it's so important to have it
+    AttributeKey() {
+
+    }
+
+    //How VirtualDOM works in React
+    VirtualDom() {
+
+    }
+
+    //React doc - https://reactjs.org/docs/getting-started.html
+    //(all info is included)
+
+
+    //Web:
+    //Cookie structure ? https ://flaviocopes.com/cookies/
+    CookieStructure() {
+
+    }
+
+    //Sessionstorage vs localstorage
+    SessionLocalStorage() {
+
+    }
+
+    //CORS
+    Cors() {
+
+    }
+
+}
